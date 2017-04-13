e62fd618b9c16e02fb6a05eba86d64344f521761
Ben Weissmann <benweissmann@gmail.com>
2017-04-12 20:38:24 -0400

    Cleanup and docs

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const Table = require('cli-table');
 const fs = require('fs');
 const child_process = require('child_process');
 
+// Logging setup
 const Console = console.Console;
-
 const stdout = new Console(process.stdout);
 const stderr = new Console(process.stderr);
 
+// Command-line args
 const HELP = `
 Usage: oplog-tailer.js [MONGO URL]
 
@@ -23,30 +24,45 @@ if ((process.argv[2] === '--help') || (process.argv[2] === '-h')) {
   process.exit(0);
 }
 
+// Set up oplog tailing
 const oplogUrl = process.argv[2] || process.env.MONGO_OPLOG_URL || 'mongodb://localhost';
 const oplog = MongoOplog(oplogUrl, {}).tail();
 
+// [database.table] -> [size in bytes]
 const totalSizePerDb = {};
+
+// [database.table] -> [number of records]
 const totalCountPerDb = {};
+
+// [table] -> [size in bytes]
 const totalSizeAcrossDbs = {};
+
+// [table] -> [number of records]
 const totalCountAcrossDbs = {};
 
+// handle new oplog entries
 oplog.on('op', data => {
   const date = new Date(data.ts.getHighBits() * 1000).toISOString().replace(/T/, ' ').replace(/\..+/, '');
   const jsonData1 = JSON.stringify(data.o) || '';
   const jsonData2 = JSON.stringify(data.o2) || '';
 
+  // print a summary of the entry
   stdout.log(`[${date}] [${_.padEnd(data.ns, 30)}] ${data.op} ${_.truncate(jsonData1, { length: 100 })} ${_.truncate(jsonData2, { length: 100 })}`);
 
+  // estimate size of update by the length of the json encoded version
   const size = jsonData1.length + jsonData2.length;
+
+  // accumulate the per-db aggregates
   totalSizePerDb[data.ns] = (totalSizePerDb[data.ns] || 0) + size;
   totalCountPerDb[data.ns] = (totalCountPerDb[data.ns] || 0) + 1;
 
+  // accumulate the cross-db aggregates
   const collectionName = data.ns.substr(data.ns.indexOf('.') + 1);
   totalSizeAcrossDbs[collectionName] = (totalSizeAcrossDbs[collectionName] || 0) + size;
   totalCountAcrossDbs[collectionName] = (totalCountAcrossDbs[collectionName] || 0) + 1;
 });
 
+// Converts a number of bytes to a human-readable estimate to 2 decimal places
 // from: http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
 function formatBytes(bytes, decimals) {
   if (bytes == 0) {
@@ -61,6 +77,7 @@ function formatBytes(bytes, decimals) {
   return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
 
+// Print a summary table
 function printTable(title, sizeData, countData) {
   stderr.log(title);
   const table = new Table({
@@ -82,6 +99,7 @@ function printTable(title, sizeData, countData) {
   stderr.log('\n\n');
 }
 
+// Print per-db and cross-db summaries and exit
 function printSummaryAndExit(status) {
   printTable('By Database', totalSizePerDb, totalCountPerDb);
   printTable('Across Databases', totalSizeAcrossDbs, totalCountAcrossDbs);
@@ -89,6 +107,7 @@ function printSummaryAndExit(status) {
   process.exit(status || 0);
 }
 
+// handle oplog tailing failures
 oplog.on('end', () => {
   stderr.log('Stream ended');
   printSummaryAndExit(1);
@@ -99,9 +118,11 @@ oplog.on('error', err => {
   printSummaryAndExit(1);
 });
 
+// Handle ctrl+c
 process.on('SIGINT', function() {
   stderr.log('Exiting due to interrupt');
   printSummaryAndExit();
 });
 
+// Print something so the user know we're live
 stderr.log('Oplog monitoring started. Entries will be printed here. Press ctrl+c to exit and print a summary.')
